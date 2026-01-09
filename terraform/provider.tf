d36312fb1a72fc481d3e524cfec8a67e0c3dbe23
ajit <ajitdeshpande00.ad@gmail.com>
2026-01-09 13:51:35 +0530

    updated bucket name

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -6,7 +6,7 @@ terraform {
     }
   }
     backend "s3" {
-    bucket         = "terraform-state-bucket-petclinic"
+    bucket         = "tf-bucket-petclinic"
     key            = "springboot/dev/terraform.tfstate"
     region         = "us-east-1"
     encrypt        = true
